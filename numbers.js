0f88cf4966599f69d80e5389387b1ac243bc129d
ritikdes <franzzbonaparte@gmail.com>
2025-01-19 21:34:33 +0545

    Delete numbers.js

--- numbers.js
+++
@@ -1,4 +0,0 @@
-/**
-Write a program that checks if a number is positive, negative, or zero and logs the result.
-Write a function that takes three numbers as input and prints the largest of the three
- */
